4629cbdc113e2430697557301861d64f26515d5e
wjy <614741930@qq.com>
2020-04-01 16:31:27 +0800

    合并

---
+++ src/pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title" @click="toGoods">{{title}}111111</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			toGoods() {
+				uni.navigateTo({url: '/pages/index/goods'});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
